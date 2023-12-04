9bdb7f676fbdf49193f6d4e66a5ca2fd3bb7d6d1
Pavel Siemko <pavelsiemko@gmail.com>
2023-12-04 19:39:27 +0100

    Create ASP.NET Core Web API solution and project for Eshop_api

---
+++ Eshop/Controllers/IsAliveController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eshop.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IsAliveController : ControllerBase
+    {
+
+        private readonly ILogger<IsAliveController> _logger;
+
+        public IsAliveController(ILogger<IsAliveController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "IsAlive")]
+        public bool Get()
+        {
+            return true;
+        }
+    }
+}
