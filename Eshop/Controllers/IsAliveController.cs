065bebd1636553a36e7445585ada63aa8637230f
Pavel Siemko <pavelsiemko@gmail.com>
2023-12-04 21:24:30 +0100

    Add swagger API v1 definition

--- Eshop/Controllers/IsAliveController.cs
+++ Eshop/Controllers/IsAliveController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Eshop.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("v1/[controller]")]
     public class IsAliveController : ControllerBase
     {
 
--- Eshop/Program.cs
+++ Eshop/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace Eshop
 {
     public class Program
@@ -12,7 +14,20 @@ namespace Eshop
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Eshop REST API v1",
+                    Description = "Eshop REST Api for get the product, get list of products and for update the product.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "GitHub",
+                        Url = new Uri("https://github.com/PavelSiemko/eshop_api")
+                    }
+                });
+            });
 
             var app = builder.Build();
 
@@ -20,7 +35,9 @@ namespace Eshop
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(options => {
+                    options.SwaggerEndpoint("v1/swagger.json", "Eshop API v1");
+                });
             }
 
             app.UseHttpsRedirection();
